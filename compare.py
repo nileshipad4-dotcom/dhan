5a5733299e5dd7edc9959d36616c00eae23bf820
nilesh raj <nileshipad4@gmail.com>
2026-01-02 07:01:19 +0530

    Update compare.py

--- compare.py
+++ compare.py
@@ -50,7 +50,7 @@ API_BASE = "https://api.dhan.co/v2"
 
 HEADERS = {
     "client-id": "1102712380",
-    "access-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzY3MzE1ODk2LCJpYXQiOjE3NjcyMjk0OTYsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTAyNzEyMzgwIn0.WpIf1CiWyqLGZB4DmTAwnRj8_sziPmkYzfxKJ-80oO9C85jeQ3JHR7YM1R4q-LySMuzViWUU32mhxK6HgjajXQ",
+    "access-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzY3NDAzNzkzLCJpYXQiOjE3NjczMTczOTMsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTAyNzEyMzgwIn0.IC5pLOp4DCPp7ge8cmmlbCyuIP0tqwKMYIkRL6U5sphsIpKPrte_Rv7RgkhjLJgi2bK7AOsO5A8ISqFWXaRZ8Q",
     "Content-Type": "application/json",
 }
 
